610501c586554e03f9caa229ffbc91f2b8dc20aa
Pham.Trong.Dai <pham.trong.dai@sun-asterisk.com>
2022-08-18 10:52:44 +0700

    Done configure datasource. Create user's model, dao, service, controller with 2 fields id, name.

--- TrainingManagementSystem/src/main/java/trainingManagementSystem/controller/HomeController.java
+++ TrainingManagementSystem/src/main/java/trainingManagementSystem/controller/HomeController.java
@@ -1,13 +1,15 @@
 package trainingManagementSystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import trainingManagementSystem.model.User;
 @Controller
 public class HomeController {
-	@RequestMapping(value  = {"/", "trang-chu"}, method = RequestMethod.GET)
-	public String index() {
+	@RequestMapping(value = { "/", "trang-chu" }, method = RequestMethod.GET)
+	public String index(ModelMap model) {
+		model.addAttribute("user", new User());
 		return "users/index";
 	}
 }
---
+++ TrainingManagementSystem/src/main/java/trainingManagementSystem/service/UserServices.java
@@ -0,0 +1,64 @@
+package trainingManagementSystem.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import trainingManagementSystem.dao.UserDao;
+import trainingManagementSystem.model.User;
+
+@Component
+@Service
+public class UserServices {
+
+	@Autowired
+	UserDao userDao;
+
+	// save user
+	public void saveUser(User user) {
+		try {
+			userDao.saveUser(user);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	// get all user
+	public List<User> getAllUser() {
+		try {
+			return userDao.getAllUser();
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	// get by id
+	public User getById(Long id, Boolean isLock) {
+		try {
+			return userDao.getById(id, isLock);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	// update user
+	public void updateUser(User user) {
+		try {
+			userDao.updateUser(user);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+	// delete User
+	public void deleteUser(Long id) {
+		try {
+			userDao.deleteUser(id);
+		} catch (Exception e) {
+			throw e;
+		}
+	}
+
+}
